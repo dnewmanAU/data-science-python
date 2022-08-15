fb33511d1f959f8a11bf6b4b9e3826dbf1f3f100
David Newman <dnewmanau@gmail.com>
2022-08-16 00:17:21 +1000

    App 1 - simple stock price using Streamlit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # data-science-python
 Data science apps built in Python
+
+## Stock Price
+
+#### Packages
+- streamlit
+- yfinance
+- pandas
+
+#### Run
+
+``python -m streamlit run stock_price.py`` or ``streamlit run stock_price.py``
---
+++ Stock Price/stock_price.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+# Markdown
+st.write("""
+# Stock Price App
+
+#### Google stock closing price and volume of shares
+
+""")
+
+# Get stock data of Google
+tickerSymbol = "GOOGL"
+tickerData = yf.Ticker(tickerSymbol)
+
+# Data point for each day since inception
+tickerDf = tickerData.history(period='1d', start="2004-1-1", end="2022-8-15")
+
+# Plot the closing price and volume of shares
+st.write("""
+### Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+### Volume of Shares
+""")
+st.line_chart(tickerDf.Volume)
